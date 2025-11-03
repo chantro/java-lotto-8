89d95ae7623cce491b77a3e2e2abdd94d76758b2
chantro <sophia2359@gmail.com>
2025-11-03 18:58:02 +0900

    feat(input): 당첨 번호와 보너스 번호 입력 단계 분리
    - 보너스 번호 입력 실패 시 당첨 번호 재입력하지 않음
    - 재입력 시 [ERROR] 메시지 출력 후 동일 단계 재시도

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,17 +2,24 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Retry;
 import lotto.domain.Lotto;
+import lotto.domain.WinningLotto;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public final class InputView {
   public static String PURCHASE_INPUT_TEXT= "구입금액을 입력해 주세요.";
+  public static String WINNING_LOTTO_INPUT_TEXT = "당첨 번호를 입력해 주세요";
+  public static String BONUS_LOTTO_INPUT_TEXT = "보너스 번호를 입력해 주세요";
   public static String NOT_INT_ERROR_MSG = "[ERROR] 숫자 형식이 아닙니다.";
   public static String NOT_POSITIVE_ERROR_MSG = "[ERROR] 양수로 입력해 주세요.";
   public static String NOT_1000_ERROR_MSG = "[ERROR] 1000원 단위로 가격을 입력하세요.";
   public static int LOTTO_PRICE = 1000;
+  public static String regex = ",";
   public static OutputView outputView;
 
   public InputView(OutputView outputView){
@@ -32,6 +39,41 @@ public final class InputView {
 
   private List<Integer> getRandomLotto(){
     return Randoms.pickUniqueNumbersInRange(1,45,6);
+  }
+
+  public WinningLotto getWinningAndBonusLotto(){
+    Lotto winning_lotto = Retry.retryUntilOk(this::getWinningLotto);
+    int bonus_lotto = Retry.retryUntilOk(this::getBonusLotto);
+    return new WinningLotto(winning_lotto, bonus_lotto);
+  }
+
+  public Lotto getWinningLotto(){
+    System.out.println(WINNING_LOTTO_INPUT_TEXT);
+    return new Lotto(validateWinningLotto(Console.readLine()));
+  }
+
+  public int getBonusLotto(){
+    System.out.println(BONUS_LOTTO_INPUT_TEXT);
+    return validateBonusLotto(Console.readLine());
+  }
+
+  private List<Integer> validateWinningLotto(String winning_lotto_str){
+    try{
+      return Arrays.stream(winning_lotto_str.split(regex, -1))
+              .map(String::trim)
+              .map(Integer::parseInt)
+              .collect(Collectors.toList());
+    }catch (NumberFormatException nfe){
+      throw new IllegalArgumentException(NOT_INT_ERROR_MSG);
+    }
+  }
+
+  private int validateBonusLotto(String bonus_lotto_str){
+    try{
+      return Integer.parseInt(bonus_lotto_str.trim());
+    }catch (NumberFormatException nfe){
+      throw new IllegalArgumentException(NOT_INT_ERROR_MSG);
+    }
   }
 
   private int validatePurchase(String purchase_str){
