53139c700fc427f6cc195c0d8fefa949ca4fd158
chantro <sophia2359@gmail.com>
2025-11-03 19:08:12 +0900

    feat(lotto): 로또 번호 검증 및 정렬 기능 추가
    - 6개 숫자 입력 여부 검증
    - 1~45 범위 확인
    - 중복 번호 금지
    - 오름차순 정렬

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers.stream().sorted().toList();
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        if (numbers.stream().anyMatch(number -> number < 1 || number > 45)){
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 범위는 1~45 이어야 합니다.");
+        }
+        if (new HashSet<>(numbers).size() != 6){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 각각 서로 달라야합니다. (중복 금지)");
+        }
+    }
+
+    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers(){
+        return numbers;
+    }
+}
