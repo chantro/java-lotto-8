54f5a673db57e87c76f8a1a8179619d81960f4af
chantro <sophia2359@gmail.com>
2025-11-03 19:15:45 +0900

    test(lotto): 로또 번호 검증 단위 테스트 추가
    - 개수, 범위, 중복 여부 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,12 @@
 package lotto;
 
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -22,4 +24,19 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    @DisplayName("로또 번호에 범위 밖의 숫자가 있으면 예외가 발생한다.")
+    void 로또_번호에_범위_밖의_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("정상 입력이면 객체가 생성된다")
+    void valid_numbers_constructs() {
+        Lotto lotto = new Lotto(List.of(1, 5, 10, 20, 30, 45));
+        assertThat(lotto.getNumbers()).hasSize(6);
+    }
 }
