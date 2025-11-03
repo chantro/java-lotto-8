ec466a7edfe0ddb0f071011da93322444d5ec7d4
chantro <sophia2359@gmail.com>
2025-11-03 23:47:15 +0900

    fix(input): retry 부분 수정
    - 중복된 값, 범위 외의 보너스 숫자 입력시에도 retry 하도록 수정

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -3,6 +3,8 @@ package lotto.domain;
 public class WinningLotto {
   private final Lotto hit_lotto;
   private final int bonus_lotto;
+  private static final String BONUS_DUP_ERROR_MSG = "[ERROR] 보너스 숫자가 당첨번호와 중복됩니다.";
+  private static final String BONUS_RANGE_ERROR_MSG = "[ERROR] 보너스 숫자는 1~45 범위여야 합니다.";
 
   public WinningLotto(Lotto hit_lotto, int bonus_lotto){
     validate(hit_lotto, bonus_lotto);
@@ -10,11 +12,11 @@ public class WinningLotto {
     this.bonus_lotto = bonus_lotto;
   }
   public void validate(Lotto hit_lotto, int bonus_lotto){
-    if (hit_lotto.getNumbers().contains(bonus_lotto)){
-      throw new IllegalArgumentException("[ERROR] 보너스 숫자가 당첨번호와 중복됩니다.");
-    }
     if (bonus_lotto < 1 || bonus_lotto > 45){
-      throw new IllegalArgumentException("[ERROR] 보너스 숫자는 1~45 범위여야 합니다.");
+      throw new IllegalArgumentException(BONUS_RANGE_ERROR_MSG);
+    }
+    if (hit_lotto.getNumbers().contains(bonus_lotto)){
+      throw new IllegalArgumentException(BONUS_DUP_ERROR_MSG);
     }
   }
 
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -12,12 +12,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public final class InputView {
-  public static String PURCHASE_INPUT_TEXT= "구입금액을 입력해 주세요.";
-  public static String WINNING_LOTTO_INPUT_TEXT = "당첨 번호를 입력해 주세요";
-  public static String BONUS_LOTTO_INPUT_TEXT = "보너스 번호를 입력해 주세요";
-  public static String NOT_INT_ERROR_MSG = "[ERROR] 숫자 형식이 아닙니다.";
-  public static String NOT_POSITIVE_ERROR_MSG = "[ERROR] 양수로 입력해 주세요.";
-  public static String NOT_1000_ERROR_MSG = "[ERROR] 1000원 단위로 가격을 입력하세요.";
+  public static final String PURCHASE_INPUT_TEXT= "구입금액을 입력해 주세요.";
+  public static final String WINNING_LOTTO_INPUT_TEXT = "당첨 번호를 입력해 주세요";
+  public static final String BONUS_LOTTO_INPUT_TEXT = "보너스 번호를 입력해 주세요";
+  public static final String NOT_INT_ERROR_MSG = "[ERROR] 숫자 형식이 아닙니다.";
+  public static final String NOT_POSITIVE_ERROR_MSG = "[ERROR] 양수로 입력해 주세요.";
+  public static final String NOT_1000_ERROR_MSG = "[ERROR] 1000원 단위로 가격을 입력하세요.";
   public static int LOTTO_PRICE = 1000;
   public static String regex = ",";
   public static OutputView outputView;
@@ -42,24 +42,24 @@ public final class InputView {
   }
 
   public WinningLotto getWinningAndBonusLotto(){
-    Lotto winning_lotto = Retry.retryUntilOk(this::getWinningLotto);
-    int bonus_lotto = Retry.retryUntilOk(this::getBonusLotto);
-    return new WinningLotto(winning_lotto, bonus_lotto);
+    Lotto hit_lotto = Retry.retryUntilOk(this::getHitLotto);
+    return Retry.retryUntilOk(() -> getWinningLotto(hit_lotto));
   }
 
-  public Lotto getWinningLotto(){
+  public Lotto getHitLotto(){
     System.out.println(WINNING_LOTTO_INPUT_TEXT);
     return new Lotto(validateWinningLotto(Console.readLine()));
   }
 
-  public int getBonusLotto(){
+  public WinningLotto getWinningLotto(Lotto hit_lotto){
     System.out.println(BONUS_LOTTO_INPUT_TEXT);
-    return validateBonusLotto(Console.readLine());
+    int bonus_lotto = validateBonusLotto(Console.readLine());
+    return new WinningLotto(hit_lotto, bonus_lotto);
   }
 
-  private List<Integer> validateWinningLotto(String winning_lotto_str){
+  private List<Integer> validateWinningLotto(String winning_lotto){
     try{
-      return Arrays.stream(winning_lotto_str.split(regex, -1))
+      return Arrays.stream(winning_lotto.split(regex, -1))
               .map(String::trim)
               .map(Integer::parseInt)
               .collect(Collectors.toList());
@@ -68,9 +68,9 @@ public final class InputView {
     }
   }
 
-  private int validateBonusLotto(String bonus_lotto_str){
+  private int validateBonusLotto(String bonus_lotto){
     try{
-      return Integer.parseInt(bonus_lotto_str.trim());
+      return Integer.parseInt(bonus_lotto.trim());
     }catch (NumberFormatException nfe){
       throw new IllegalArgumentException(NOT_INT_ERROR_MSG);
     }
